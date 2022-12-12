49670f5f9792a208cedf7fecf5731597ab54e7db
Shogo Kurokawa <198235@st.takushoku-u.ac.jp>
2022-12-12 15:07:02 +0900

    HTJ2K integration is done

---
+++ codec.h
@@ -0,0 +1,48 @@
+// #include <stddef.h>
+
+#ifndef HTJ2K_CODEC_H
+#define HTJ2K_CODEC_H
+
+#include <cstdint>
+
+namespace htj2k {
+
+struct CodestreamBuffer {
+  uint8_t* codestream;
+  size_t size;
+  uint8_t* init_data;
+  size_t init_data_size;
+
+  CodestreamBuffer() : codestream(NULL), size(0), init_data(NULL), init_data_size(0) {}
+};
+
+struct PixelBuffer {
+  uint32_t height;
+  uint32_t width;
+  uint8_t num_comps;
+  uint8_t* pixels;
+};
+
+class Encoder {
+ public:
+  virtual CodestreamBuffer encodeRGB8(const uint8_t* pixels, uint32_t width, uint32_t height) = 0;
+
+  virtual CodestreamBuffer encodeRGBA8(const uint8_t* pixels, uint32_t width, uint32_t height) = 0;
+
+  virtual ~Encoder() {}
+};
+
+class Decoder {
+ public:
+  virtual PixelBuffer decodeRGB8(const uint8_t* CodestreamBuffer, size_t size, uint32_t width,
+                                 uint32_t height, const uint8_t* init_data, size_t init_data_size) = 0;
+
+  virtual PixelBuffer decodeRGBA8(const uint8_t* codestream, size_t size, uint32_t width, uint32_t height,
+                                  const uint8_t* init_data, size_t init_data_size) = 0;
+
+  virtual ~Decoder() {}
+};
+
+};  // namespace htj2k
+
+#endif
