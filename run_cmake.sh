42c42ab9e0978264c05a7f469eec28e7ecb11f3e
Osamu Watanabe <owatanab@es.takushoku-u.ac.jp>
2024-10-24 16:06:36 +0900

    Fix run_cmake.sh

--- run_cmake.sh
+++ run_cmake.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cmake -B./build -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -DCMAKE_LINKER=/usr/bin/ld.lld-16
+cmake -B./build -G Ninja -DCMAKE_BUILD_TYPE=Release
